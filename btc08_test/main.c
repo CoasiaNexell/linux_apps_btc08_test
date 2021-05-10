89e25bd7498cfebe43ba4bcca0e66ffd2ca3850e
michelle.lee@coasia.com <michelle.lee@coasianexell.com>
2021-05-10 19:52:13 +0900

    btc08_test:  Handle hash/vtk board related GPIOs
    This patch defines GPIOs to read board type and board connection status
    and to enable FAN.
    
    Signed-off-by: michelle.lee@coasia.com <michelle.lee@coasianexell.com>
    Change-Id: I2797e10438ddfbd97ea1162c6a1312b6c27e820e

--- btc08_test/main.c
+++ btc08_test/main.c
@@ -2,6 +2,7 @@
 #include <strings.h>
 #include "TestFunction.h"
 #include "Utils.h"
+#include "GpioControl.h"
 
 #include "SingleCommand.h"
 #include "SimpleWork.h"
@@ -13,6 +14,10 @@
 #endif
 #define NX_DTAG "[MAIN]"
 #include "NX_DbgMsg.h"
+
+#define GPIO_HASH0_PLUG         (24)		// High: Hash0 connected, Low: Hash0 removed
+#define GPIO_HASH0_BODDET       (20)		// High: Hash0, Low: VTK
+#define GPIO_HASH0_PWREN         (0)		// High: FAN ON, Low : FAN OFF
 
 static void l1_command_liist()
 {
@@ -27,11 +32,44 @@ static void l1_command_liist()
 	printf("=============================\n");
 }
 
+void setup_hashboard_gpio()
+{
+	int plug_status = 0;
+	int board_type = 0;
+
+	GPIO_HANDLE hPlug  = NULL;
+	GPIO_HANDLE hBdDET = NULL;
+	GPIO_HANDLE hPwrEn = NULL;
+
+	hPlug  = CreateGpio(GPIO_HASH0_PLUG);
+	hBdDET = CreateGpio(GPIO_HASH0_BODDET);
+	hPwrEn = CreateGpio(GPIO_HASH0_PWREN);
+
+	GpioSetDirection(hPlug,  GPIO_DIRECTION_IN);
+	GpioSetDirection(hBdDET, GPIO_DIRECTION_IN);
+	GpioSetDirection(hPwrEn, GPIO_DIRECTION_OUT);
+
+	plug_status = GpioGetValue(hPlug);
+	board_type  = GpioGetValue(hBdDET);
+	GpioSetValue(hPwrEn, 1);
+
+	printf("Hash0: connection status(%s), board_type(%s)\n",
+			(plug_status == 1) ? "Connected":"Removed",
+			(board_type  == 1) ? "Hash":"VTK");
+
+	if( hPlug  )	DestroyGpio( hPlug );
+	if( hBdDET )	DestroyGpio( hBdDET );
+	if( hPwrEn )	DestroyGpio( hPwrEn );
+}
+
 int main( int argc, char *argv[] )
 {
 	static char cmdStr[NX_SHELL_MAX_ARG * NX_SHELL_MAX_STR];
 	static char cmd[NX_SHELL_MAX_ARG][NX_SHELL_MAX_STR];
 	int cmdCnt;
+
+	setup_hashboard_gpio();
+
 	for( ;; )
 	{
 		l1_command_liist();
