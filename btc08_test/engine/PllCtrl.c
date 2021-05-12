40740d82a92839c0d800f3e217bfe9c05b5646a1
michelle.lee@coasia.com <michelle.lee@coasianexell.com>
2021-05-12 22:01:26 +0900

    btc08-test: Add DisableCore test
    This patch adds a single command test to test bist and mining with some
    cores disabled.
    
    Signed-off-by: michelle.lee@coasia.com <michelle.lee@coasianexell.com>
    Change-Id: I75cbb04d9fc294aab825d5fb71fad4e81d354b29

--- btc08_test/engine/PllCtrl.c
+++ btc08_test/engine/PllCtrl.c
@@ -62,3 +62,47 @@ int GetPllIdx2Freq(int pll_idx)
     else
         return pll_sets[pll_idx].freq;
 }
+
+void DumpPllValue(uint8_t val[4])
+{
+// [31:29] : reserved
+// [28]    : FSEL : 0
+// [27]    : FEED_EN : 0
+// [26:22] : EXTAFC : 5'h0
+// [21]    : AFC_ENB : 0
+// [20]    : DIV_SEL : 1
+// [19]    : BYPASS : 0
+// [18:16] : S : 3'h1, 3’b001
+// [15:6]  : M : 10'h21C, 10’b10_0001_1100
+// [5:0]   : P : 6'h06,6’b00_0110
+	uint32_t P, M, S;
+	uint32_t BYPASS;
+	uint32_t DIV_SEL;
+	uint32_t AFC_ENB;
+	uint32_t EXTAFC;
+	uint32_t FEED_EN;
+	uint32_t F_SEL;
+	uint32_t Reseved;
+
+	P       =  val[3] & 0x3f;
+	M       = (val[2] << 2) | ((val[3] * 0xC0)>>6);
+	S       =  val[1] & 0x7;
+	BYPASS  = (val[1] & 0x08) >> 3;
+	DIV_SEL = (val[1] & 0x10) >> 4;
+	AFC_ENB = (val[1] & 0x20) >> 5;
+	EXTAFC  = ((val[0] & 0x7)<<2) | (val[0] & 0xC0)>>6;
+	FEED_EN = (val[0] & 0x8)>>3;
+	F_SEL   = (val[0] & 0x10)>>4;
+
+	NxDbgMsg(NX_DBG_DEBUG, "================== 0x%02x 0x%02x 0x%02x 0x%02x\n", val[0], val[1], val[2], val[3]);
+	NxDbgMsg(NX_DBG_DEBUG, "P       = %x(%d) \n", P, P );
+	NxDbgMsg(NX_DBG_DEBUG, "M       = %x(%d) \n", M, M );
+	NxDbgMsg(NX_DBG_DEBUG, "S       = %x(%d) \n", S, S );
+	NxDbgMsg(NX_DBG_DEBUG, "BYPASS  = %x \n", BYPASS  );
+	NxDbgMsg(NX_DBG_DEBUG, "DIV_SEL = %x \n", DIV_SEL );
+	NxDbgMsg(NX_DBG_DEBUG, "AFC_ENB = %x \n", AFC_ENB );
+	NxDbgMsg(NX_DBG_DEBUG, "EXTAFC  = %x \n", EXTAFC  );
+	NxDbgMsg(NX_DBG_DEBUG, "FEED_EN = %x \n", FEED_EN );
+	NxDbgMsg(NX_DBG_DEBUG, "F_SEL   = %x \n", F_SEL   );
+	NxDbgMsg(NX_DBG_DEBUG, "==================\n");
+}
